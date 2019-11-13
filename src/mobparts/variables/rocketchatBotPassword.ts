2c905df0ee0e4dce9c7fd6bfb9ceccdd90568ccd
George <georgegeorgulas@gmail.com>
2019-11-13 02:50:15 -0500

    something or other

---
+++ src/mobparts/variables/rocketchatBotPassword.ts
@@ -0,0 +1,14 @@
+//  rocketchatBotPassword.ts
+//  MegaDocker
+//  The variable for
+//  Created by George Georgulas IV on 1/26/19.
+//  Copyright © 2019 The MegaDocker Group. All rights reserved.
+
+import { IMemory } from '../../classes/IMemory';
+import { rocketchatBotUsername } from '../memories/rocketchatBotUsername';
+
+export let rocketchatBotPassword: IMemory = {
+  name: `RocketChat bot user name`,
+  tooltip: `Choose the password of the RocketChat bot user (${rocketchatBotUsername.value}).`,
+  value: ``
+};
