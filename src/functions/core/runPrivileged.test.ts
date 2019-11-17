262b97c1fffe76df87a8d6037408eccc5ae3dda5
George <georgegeorgulas@gmail.com>
2019-11-17 14:24:28 -0500

    fixed test

--- src/functions/core/runPrivileged.test.ts
+++ src/functions/core/runPrivileged.test.ts
@@ -1,1 +1,7 @@
 import { runPrivileged } from './runPrivileged';
+import { run } from './run';
+
+it(`should return ls for /root`, () => {
+  expect(run('ls', ['-la', '/root'])).toThrowError;
+  expect(runPrivileged('ls', ['-la', '/root'])).toReturn;
+});
