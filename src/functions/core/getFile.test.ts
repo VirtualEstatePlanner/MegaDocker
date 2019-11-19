8e9a9dee751f2c7dc6481aa0ab0e5135e7378e54
George <georgegeorgulas@gmail.com>
2019-11-18 22:54:09 -0500

    hooray for async tests!

--- src/functions/core/getFile.test.ts
+++ src/functions/core/getFile.test.ts
@@ -1,22 +1,36 @@
-import { getFile } from './getFile';
 import { checkForFile } from './checkForFile';
 import { deleteFile } from './deleteFile';
-import { run } from './run';
+import { getFile } from './getFile';
+import { doesNotReject } from 'assert';
 
-it(`should download a URL to a file`, () => {
-  console.log(`checking for 'downloadedfile.html' in ${process.env.PWD}`);
-  console.log(checkForFile(`${process.env.PWD}`, `downloadedfile.html`));
-  expect(checkForFile(`${process.env.PWD}`, `downloadedfile.html`)).toBeFalsy;
-  const downloadURL: URL = new URL(
-    `https://github.com/VirtualEstatePlanner/MD/blob/master/README.md`
+it(`should download a URL to a file`, async () => {
+  console.log(
+    `this should be false: ${checkForFile(
+      `${process.env.PWD}`,
+      `downloadedfile.md`
+    )}`
   );
-  console.log(`downloading 'downloadedfile.html' to ${process.env.PWD}`);
-  getFile(`${process.env.PWD}`, `downloadedfile`, `html`, downloadURL);
-  run(`sleep 15`);
-  console.log(`checking for 'downloadedfile.html' in ${process.env.PWD}`);
-  expect(checkForFile(`${process.env.PWD}`, `downloadedfile.html`)).toBeTruthy;
-  console.log(checkForFile(`${process.env.PWD}`, `downloadedfile.html`));
-  deleteFile(`${process.env.PWD}`, `downloadedfile.html`);
-  console.log(`checking for 'downloadedfile.html' in ${process.env.PWD}`);
-  expect(checkForFile(`${process.env.PWD}`, `downloadedfile.html`)).toBeFalsy;
+  expect(checkForFile(`${process.env.PWD}`, `downloadedfile.md`)).toBeFalsy;
+  const downloadURL: string = `https://raw.githubusercontent.com/VirtualEstatePlanner/CRATE/master/README.md`;
+  try {
+    await getFile(`${process.env.PWD}`, `downloadedfile.md`, downloadURL);
+  } catch (e) {
+    expect(e).toMatch('error');
+  }
+
+  console.log(
+    `this should be true: ${checkForFile(
+      `${process.env.PWD}`,
+      `downloadedfile.md`
+    )}`
+  );
+  expect(checkForFile(`${process.env.PWD}`, `downloadedfile.md`)).toBeTruthy;
+  deleteFile(`${process.env.PWD}`, `downloadedfile.md`);
+  console.log(
+    `this should be false: ${checkForFile(
+      `${process.env.PWD}`,
+      `downloadedfile.md`
+    )}`
+  );
+  expect(checkForFile(`${process.env.PWD}`, `downloadedfile.md`)).toBeFalsy;
 });
--- src/functions/core/getFile.ts
+++ src/functions/core/getFile.ts
@@ -4,20 +4,62 @@
 //  Created by George Georgulas IV on 1/26/19.
 //  Copyright © 2019 The MegaDocker Group. All rights reserved.
 
-import { run } from './run';
+import { createWriteStream, WriteStream } from 'fs';
+import { IncomingMessage } from 'http';
+import { get } from 'https';
+import { parse, UrlWithStringQuery } from 'url';
 
 /**
- * @param fileName: string describing the name of the saved file
- * @param fileExtension: string describing the extension of the saved file
- * @param pathTo: string describing the path to the saved file
+ * @param pathTo: string containing the path to the saved file
+ * @param fileName: string containing the name and extension of the file
  * @param url: URL to be downloaded
  * downloads a URL to a local file
  */
-export function getFile(
+
+const TIMEOUT = 10000;
+
+function getSomeFile(pathTo: string, fileName: string, urlToDownload: string) {
+  const uri: UrlWithStringQuery = parse(urlToDownload);
+  const file: WriteStream = createWriteStream(`${pathTo}/${fileName}`);
+
+  return new Promise(function(resolve, reject) {
+    const request = get(uri.href).on('response', function(
+      res: IncomingMessage
+    ) {
+      res
+        .on('data', function(chunk) {
+          file.write(chunk);
+          console.log(`wrote a chunk`);
+        })
+        .on('end', function() {
+          file.end();
+          console.log(`download completed`);
+          resolve();
+        })
+        .on('error', function(err) {
+          reject(err);
+        });
+    });
+    request.setTimeout(TIMEOUT, function() {
+      request.abort();
+      reject(new Error(`request timeout after ${TIMEOUT / 1000.0}s`));
+    });
+  });
+}
+
+export async function getFile(
   pathTo: string,
   fileName: string,
-  fileExtension: string,
-  url: URL
-): void {
-  run(`curl -q ${url} --output ${pathTo}/${fileName}.${fileExtension}`, ['']);
+  urlToDownload: string
+) {
+  console.log(
+    `downloading file '${pathTo}/${fileName} from: \n${urlToDownload}`
+  );
+
+  try {
+    await getSomeFile(pathTo, fileName, urlToDownload);
+    console.log(`Download completed`);
+  } catch (e) {
+    console.log(`Download failed with ${e.message}`);
+  }
 }
