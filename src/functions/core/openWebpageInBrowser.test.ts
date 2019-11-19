c766290bf9528e495173e327e3faa49080772ec9
George <georgegeorgulas@gmail.com>
2019-11-18 23:42:26 -0500

    webpages now open

--- src/functions/core/openWebpageInBrowser.test.ts
+++ src/functions/core/openWebpageInBrowser.test.ts
@@ -1,1 +1,17 @@
 import { openWebpageInBrowser } from './openWebpageInBrowser';
+
+it(`opens a webpage in the default browser`, async () => {
+  try {
+    console.log(`opening google`);
+    await openWebpageInBrowser(`https://news.google.com`);
+  } catch (e) {
+    expect(e).toMatch('error');
+  }
+
+  console.log(`opening appleinsider`);
+  try {
+    openWebpageInBrowser(`https://www.appleinsider.com`);
+  } catch (e) {
+    expect(e).toMatch('error');
+  }
+});
--- src/functions/core/openWebpageInBrowser.ts
+++ src/functions/core/openWebpageInBrowser.ts
@@ -4,11 +4,13 @@
 //  Created by George Georgulas IV on 1/26/19.
 //  Copyright © 2019 The MegaDocker Group. All rights reserved.
 
+import opener from 'opener';
+
 /**
  * @param urlString: a string representing a URL to be
  * opened in the external browser application
  * opens the given URL as a webpage in the default browser
  */
 export function openWebpageInBrowser(urlString: string): void {
-  window.open(urlString);
+  opener(urlString);
 }
