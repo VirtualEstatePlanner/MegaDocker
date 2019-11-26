887f0e2d4e7d5389de0a040d5c8fdecdadf3a178
George <georgegeorgulas@gmail.com>
2019-11-26 12:32:50 -0500

    attempt to fix VBox installation

--- src/functions/modules/installVBoxForMac.test.ts
+++ src/functions/modules/installVBoxForMac.test.ts
@@ -1,1 +1,23 @@
 import { installVBoxForMac } from './installVBoxForMac';
+import { downloadVBoxForMac } from './downloadVBoxForMac';
+import { checkForFile } from '../core/checkForFile';
+import { downloadFolder } from '../../globals/downloadFolder';
+import { run } from '../core/run';
+
+it(`should install VirtualBox`, async () => {
+  jest.setTimeout(120000);
+  /*console.log(`downloading VBox dmg`);
+  expect(checkForFile(downloadFolder, `VirtualBox.dmg`)).toBeFalsy;
+  try {
+    downloadVBoxForMac();
+  } catch (e) {
+    expect(e).toMatch('error');
+  }*/
+  console.log(`installing VBox`);
+  try {
+    installVBoxForMac();
+  } catch (e) {
+    //    expect(e).toMatch('error');
+  }
+  expect(run(`VBoxManage`, [``])).toReturn;
+});
--- src/functions/modules/installVBoxForMac.ts
+++ src/functions/modules/installVBoxForMac.ts
@@ -11,21 +11,31 @@ import { runPrivilegedOrdered } from '../core/runPrivilegedOrdered';
 /**
  * installs VirtualBox for Mac from the .dmg file
  */
-export function installVBoxForMac(): void {
-  if (checkForFile(downloadFolder, 'VirtualBox.dmg') === true) {
-    runPrivilegedOrdered('/usr/bin/hdiutil', ['attach', '/tmp/VirtualBox.dmg']);
-    runOrdered(`sleep(30)`);
-    runPrivilegedOrdered('/usr/sbin/installer', [
-      '-pkg',
-      '/Volumes/VirtualBox/VirtualBox.pkg',
-      '-target',
-      '/'
+function doInstallation(): void {
+  if (checkForFile(downloadFolder, `VirtualBox.dmg`) === true) {
+    console.log(`installing VBox from disk image`);
+    runPrivilegedOrdered(`/usr/bin/hdiutil`, [
+      `attach`,
+      `/${downloadFolder}/VirtualBox.dmg`
     ]);
     runOrdered(`sleep(30)`);
-    runPrivilegedOrdered('/usr/sbin/diskutil', [
-      'eject',
-      '/Volumes/VirtualBox'
+    runPrivilegedOrdered(`/usr/sbin/installer`, [
+      `-pkg`,
+      `/Volumes/VirtualBox/VirtualBox.pkg`,
+      `-target`,
+      `/`
+    ]);
+    runOrdered(`sleep(30)`);
+    runPrivilegedOrdered(`/usr/sbin/diskutil`, [
+      `eject`,
+      `/Volumes/VirtualBox`
     ]);
     // TODO needs a return that states whether or not it succeeded
   }
+}
+
+export async function InstallVBoxForMac() {
+  try {
+    await doInstallation();
+  } catch (e) {}
 }
