eac395e8d878e03d1dbf85a4a8a74000f8b58c15
George <georgegeorgulas@gmail.com>
2020-01-21 12:52:40 -0500

    removed comment

--- src/components/ManikinInfo.tsx
+++ src/components/ManikinInfo.tsx
@@ -3,7 +3,6 @@ import Card from "@material-ui/core/Card"
 import CardContent from "@material-ui/core/CardContent"
 import { StoreProvider } from "./Store";
 
-// placeholder data
 const info: string = ``
 
 export const ManikinInfo: React.FC = () => {
