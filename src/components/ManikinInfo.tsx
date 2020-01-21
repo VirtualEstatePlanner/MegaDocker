1df30e19ba4dfc307a2abc156086e3fa6f8f5893
George <georgegeorgulas@gmail.com>
2020-01-21 12:49:57 -0500

    imported StoreProvider

--- src/components/ManikinInfo.tsx
+++ src/components/ManikinInfo.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Card from "@material-ui/core/Card"
 import CardContent from "@material-ui/core/CardContent"
+import { StoreProvider } from "./Store";
 
 // placeholder data
 const info: string = ``
 
 export const ManikinInfo: React.FC = () => {
     return (
-        <React.Suspense fallback={<div>...loading</div>}>
-            <Card className="ManikinInfo">
-                <CardContent>{info}</CardContent>
-            </Card>
-        </React.Suspense>)
+        <StoreProvider>
+            <React.Suspense fallback={<div>...loading</div>}>
+                <Card className="ManikinInfo">
+                    <CardContent>{info}</CardContent>
+                </Card>
+            </React.Suspense>
+        </StoreProvider>)
 }
