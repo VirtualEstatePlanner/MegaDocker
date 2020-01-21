41a332efef4f8e45e87b11e831dd11d7f91656e3
George <georgegeorgulas@gmail.com>
2020-01-21 00:33:00 -0500

    more testing UI

--- src/components/VariableTable.tsx
+++ src/components/VariableTable.tsx
@@ -1,19 +1,25 @@
 import React from "react";
+import Input from "@material-ui/core/Input";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import Tooltip from "@material-ui/core/Tooltip";
 import { IMemory } from '../classes/IMemory';
 import { allManikins } from '../globals/allManikins';
+import { mobName } from '../mobparts/memories/mobName'
 import * as checkmarkIcon from "../images/indicators/checkmarkIcon.png"
 import * as xmarkIcon from "../images/indicators/xmarkIcon.png"//import { mobName } from "../mobparts/memories/mobName"
 
-const tableMemories: IMemory[] = allManikins.filter(eachManikin => (
+var tableMemories: IMemory[] = allManikins.filter(eachManikin => (
     eachManikin.isSelected === true)).map(eachManikin => (
         eachManikin.memories)).filter(function (value, index, array) {
             return value.length !== 0
         }).flat()
+
+tableMemories = [mobName, mobName, mobName, mobName, mobName, mobName, mobName, mobName, mobName, mobName, mobName, mobName, mobName, mobName, mobName, mobName, mobName, ...tableMemories]
+console.log(tableMemories)
 
 export const VariableTable: React.FC = () => {
     const [variableValue, setVariableValue] = React.useState();
@@ -22,25 +28,27 @@ export const VariableTable: React.FC = () => {
             <Table className="VariableTable" size="small" stickyHeader>
                 <TableHead className="VariableTableHeader" >
                     <TableRow>
-                        <TableCell>Variable Description</TableCell>
+                        <TableCell >Description</TableCell>
                         <TableCell>Value</TableCell>
-                        <TableCell>Information</TableCell>
                         <TableCell>Ready</TableCell>
                     </TableRow>
                 </TableHead>
-                <TableBody>
-
+                <TableBody className="MemoryTableBody">
                     {tableMemories.map(eachMemory => (
                         <TableRow key={eachMemory.name}>
-                            <TableCell >{eachMemory.name}</TableCell>
-                            <TableCell><input
-                                value={eachMemory.value}
-                                placeholder=""
-                                onChange={e => setVariableValue(e.target.value)} size={50} /></TableCell>
-                            <TableCell>{eachMemory.tooltip}</TableCell>
-                            <TableCell><img alt='ready indicator' height={20} width={20} src={eachMemory.value ? checkmarkIcon : xmarkIcon} /></TableCell>
+                            <TableCell padding='checkbox'>{eachMemory.name}</TableCell>
+                            <TableCell >
+                                <Tooltip title={eachMemory.tooltip}>
+                                    <Input
+                                        fullWidth
+                                        value={eachMemory.value}
+                                        placeholder="input data here"
+                                        onChange={event => setVariableValue(event.target.value)}
+                                    />
+                                </Tooltip>
+                            </TableCell>
+                            <TableCell ><img alt='ready indicator' height={20} width={20} src={eachMemory.value ? checkmarkIcon : xmarkIcon} /></TableCell>
                         </TableRow>))}
-
                 </TableBody>
             </Table>
         </React.Suspense>)
