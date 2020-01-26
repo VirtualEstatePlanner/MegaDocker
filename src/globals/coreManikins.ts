5a350dd168d7898fd6a6fec5c9c8f5c851edabf7
George <georgegeorgulas@gmail.com>
2020-01-26 16:24:11 -0500

    eliminated globals

--- src/globals/coreManikins.ts
+++
@@ -1,8 +0,0 @@
-import { allManikins } from './allManikins';
-
-/**
- * an array containing all MEGADocker's Core Manikins
- */
-export const coreManikins = allManikins.filter(
-  (eachManikin) => eachManikin.isCore === false
-);
--- src/interfaces/IMegaDockerState.ts
+++ src/interfaces/IMegaDockerState.ts
@@ -2,13 +2,7 @@ import { IManikin } from './IManikin';
 import { IMemory } from './IMemory';
 import { IMite } from './IMite';
 import { allManikins } from '../globals/allManikins';
-import { coreMemories } from '../globals/coreMemories';
-import {
-  startingMites,
-  startingServiceMites,
-  startingNetworkMites,
-  startingCustomMites
-} from '../globals/startingMites';
+
 export interface IMegaDockerState {
   manikinTableContents: IManikin[];
   memoryTableContents: IMemory[];
@@ -20,13 +14,37 @@ export interface IMegaDockerState {
   ymlOutput: string;
 }
 
+const coreManikins: IManikin[] = allManikins.filter(
+  (eachManikin) => eachManikin.isCore === true
+);
+
+console.log(coreManikins);
+
+const coreMemsArray: IMemory[][] = coreManikins.map(
+  (eachManikin) => eachManikin.memories
+);
+
+console.log(coreMemsArray);
+
+const coreMems: IMemory[] = coreMemsArray.flatMap((eachArray) =>
+  eachArray.concat().filter((eachMemory) => eachMemory)
+);
+
+console.log(coreMems);
+
+const mobMites = allManikins.flatMap((eachManikin) =>
+  eachManikin.mites.flatMap((eachMite) => eachMite)
+);
+
+console.log(mobMites);
+
 export const initialMegaDockerState: IMegaDockerState = {
   manikinTableContents: [...allManikins],
-  memoryTableContents: [...coreMemories],
-  allMobMites: [...startingMites],
-  mobServiceMites: [...startingServiceMites],
-  mobNetworkMites: [...startingNetworkMites],
-  mobCustomMites: [...startingCustomMites],
+  memoryTableContents: coreMems,
+  allMobMites: mobMites,
+  mobServiceMites: mobMites.filter((eachMite) => eachMite.type === `Service`),
+  mobNetworkMites: mobMites.filter((eachMite) => eachMite.type === `Network`),
+  mobCustomMites: mobMites.filter((eachMite) => eachMite.type === `Custom`),
   infoContent: `Learn more about the object you last clicked on by reading the text in this part of the window.`,
   ymlOutput: ``
 };
