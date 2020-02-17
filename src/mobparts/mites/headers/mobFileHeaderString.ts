b30c241dec5264fcf1aa7d2f392c1b5bb662e995
George <georgegeorgulas@gmail.com>
2020-02-17 11:20:15 -0500

    updated to docker-comopse version 3.7

--- src/mobparts/mites/headers/mobFileHeaderString.ts
+++ src/mobparts/mites/headers/mobFileHeaderString.ts
@@ -3,7 +3,7 @@ import { mobFolderPath } from '../../memories/mobFolderPath';
 
 export const mobFileHeaderString: string = `
 # MegaDocker YML File
-version: '3.6'
+version: '3.7'
 
 # Created by MegaDocker
 # Data is stored at: ${mobFolderPath}
