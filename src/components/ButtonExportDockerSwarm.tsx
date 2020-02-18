8350b35cb114e38414c5592e7d87c4d34f37cf78
George <georgegeorgulas@gmail.com>
2020-02-17 21:09:17 -0500

    removed old code

--- src/components/ButtonExportDockerSwarm.tsx
+++ src/components/ButtonExportDockerSwarm.tsx
@@ -4,15 +4,6 @@ import { MegaContext } from './MegaContext'
 import { IMegaDockerState } from '../interfaces/IMegaDockerState';
 import { IMegaDockerAction } from '../interfaces/IMegaDockerAction';
 
-//old buttonClicked code
-//saveFile(userMegaDockerFolder, mobName.value, `yml`, userMob.mobManikins.toString())
-// saveFile(
-//     `${process.env.PWD}`,
-//     `testfilename`,
-//     `yml`,
-//     `so what the hell does this yml even say?`
-// );
-
 export const ButtonExportDockerSwarm: React.FC = (): React.ReactElement => {
 
     const { state, dispatch }: { state: IMegaDockerState, dispatch: React.Dispatch<IMegaDockerAction> } = React.useContext(MegaContext)
