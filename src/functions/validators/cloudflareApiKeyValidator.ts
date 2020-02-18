e39905e2b5ab5f70d0159968642b5aadacc053c2
George <georgegeorgulas@gmail.com>
2020-02-17 20:29:22 -0500

    fix CF api validator as it changed from noon today

--- src/functions/validators/cloudflareApiKeyValidator.ts
+++ src/functions/validators/cloudflareApiKeyValidator.ts
@@ -1,7 +1,7 @@
 export const cloudflareApiKeyValidator = (
   cloudflareApiKeyString: string
 ): boolean => {
-  const isAlphanumericRegex: RegExp = new RegExp(`[a-zA-Z0-9]{40,40}`);
+  const isAlphanumericRegex: RegExp = new RegExp(`[a-zA-Z0-9_-]{40,40}`);
   const containsOnlyAlphanumerics: boolean = isAlphanumericRegex.test(
     cloudflareApiKeyString
   )
