7d4ffb6ba9412254e2200905a7d543c408a33b64
George <georgegeorgulas@gmail.com>
2020-02-17 20:25:02 -0500

    domain validation is (mostly) happy

--- src/functions/validators/domainValidator.ts
+++ src/functions/validators/domainValidator.ts
@@ -1,8 +1,6 @@
 export const domainValidator = (domain: string): boolean => {
-  const domainRegex: RegExp = new RegExp(
-    '/^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:.[a-zA-Z]{2,})+/'
-  );
-  const thisRegex: RegExp = domainRegex;
-  const isValidated: boolean = thisRegex.test(domain) ? true : false;
+  // const domainRegex: RegExp = new RegExp(``);
+  const domainRegex: RegExp = /^(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  const isValidated: boolean = domainRegex.test(domain) ? true : false;
   return isValidated;
 };
