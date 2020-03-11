3350ef8d17e19310720e5790375fd5723f52e937
George <georgegeorgulas@gmail.com>
2020-03-11 08:08:04 -0400

    corrected workingManikins list for application

--- src/globals/workingManikins.ts
+++ src/globals/workingManikins.ts
@@ -1,6 +1,6 @@
 import { IManikin } from '../interfaces/IManikin';
 // import manikins
-import { backupManikin } from '../mobparts/manikins/backup';
+// import { backupManikin } from '../mobparts/manikins/backup';
 import { drupalManikin } from '../mobparts/manikins/drupal';
 import { elkManikin } from '../mobparts/manikins/elk';
 import { emailManikin } from '../mobparts/manikins/email';
@@ -9,9 +9,9 @@ import { gitlabManikin } from '../mobparts/manikins/gitlab';
 import { heimdallManikin } from '../mobparts/manikins/heimdall';
 import { kanboardManikin } from '../mobparts/manikins/kanboard';
 import { ldapManikin } from '../mobparts/manikins/ldap';
-import { nextcloudManikin } from '../mobparts/manikins/nextcloud';
+// import { nextcloudManikin } from '../mobparts/manikins/nextcloud';
 import { nginxManikin } from '../mobparts/manikins/nginx';
-import { owncloudManikin } from '../mobparts/manikins/owncloud';
+// import { owncloudManikin } from '../mobparts/manikins/owncloud';
 import { piholeManikin } from '../mobparts/manikins/pihole';
 import { portainerManikin } from '../mobparts/manikins/portainer';
 import { rocketchatManikin } from '../mobparts/manikins/rocketchat';
@@ -19,15 +19,15 @@ import { skoposManikin } from '../mobparts/manikins/skopos';
 import { swarmpitManikin } from '../mobparts/manikins/swarmpit';
 import { traefikManikin } from '../mobparts/manikins/traefik';
 import { visualizerManikin } from '../mobparts/manikins/visualizer';
-import { vsCodeManikin } from '../mobparts/manikins/vscode';
-import { webdavManikin } from '../mobparts/manikins/webdav';
+// import { vsCodeManikin } from '../mobparts/manikins/vscode';
+// import { webdavManikin } from '../mobparts/manikins/webdav';
 import { wordpressManikin } from '../mobparts/manikins/wordpress';
 
 /**
  * an array containing all Manikins available in MEGADocker
  */
 export const workingManikins: IManikin[] = [
-  backupManikin,
+  // backupManikin,
   drupalManikin,
   elkManikin,
   emailManikin,
@@ -36,9 +36,9 @@ export const workingManikins: IManikin[] = [
   heimdallManikin,
   kanboardManikin,
   ldapManikin,
-  nextcloudManikin,
+  // nextcloudManikin,
   nginxManikin,
-  owncloudManikin,
+  // owncloudManikin,
   piholeManikin,
   portainerManikin,
   rocketchatManikin,
@@ -46,7 +46,7 @@ export const workingManikins: IManikin[] = [
   swarmpitManikin,
   traefikManikin,
   visualizerManikin,
-  vsCodeManikin,
-  webdavManikin,
+  // vsCodeManikin,
+  // webdavManikin,
   wordpressManikin
 ];
