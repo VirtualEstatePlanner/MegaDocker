d06bee1da8abdde14ea6defb09d0273b98be2c48
George <georgegeorgulas@gmail.com>
2020-05-21 13:12:33 -0400

    named changeEvent more descriptively

--- src/components/MemoryRow.tsx
+++ src/components/MemoryRow.tsx
@@ -5,11 +5,11 @@ import * as checkmarkIcon from "../images/indicators/checkmarkIcon.png"
 import * as xmarkIcon from "../images/indicators/xmarkIcon.png"
 
 export const MemoryRow: React.FC = (): React.ReactElement => {
-    const [memoryValue, setMemoryValue] = React.useState();
+    const [memoryValue, setMemoryValue]: [string | undefined, React.Dispatch<React.SetStateAction<string | undefined>>] = React.useState();
     return (
         <TableRow className="MemoryRow" hover>
             <TableCell>IMemory.name</TableCell>
-            <TableCell><input value={memoryValue} placeholder="Imemory.value" onChange={e => setMemoryValue(e.target.value)} size={50} /></TableCell>
+            <TableCell><input value={memoryValue} placeholder="IMemory.value" onChange={changeEvent => setMemoryValue(changeEvent.target.value)} size={50} /></TableCell>
             <TableCell>IMemory.tooltip</TableCell>
             <TableCell><img alt='ready indicator' height={20} width={20} src={memoryValue ? checkmarkIcon : xmarkIcon} /></TableCell>
         </TableRow>)
