40c3cf950884ec17e38cf77a3b449481689c17e9
George <georgegeorgulas@gmail.com>
2020-05-26 17:23:16 -0400

    fixed tv service mite yml spacing

--- src/mobparts/mites/service/tvServiceMite.ts
+++ src/mobparts/mites/service/tvServiceMite.ts
@@ -14,33 +14,33 @@ export const tvServiceMite: IMite = {
 # Begin TV Service Section
 
  tv:
- image: linuxserver/sonarr:latest
- networks:
-  - traefik
-  - tv
- volumes:
-  - ./media/tv-config:/config
-  - ./media/tv-data:/media
- environment:
-  - PUID=$HOSTUSERID
-  - PGID=$HOSTUSERGID
- deploy:
-  restart_policy:
-   condition: on-failure
-  labels:
-   - 'traefik.enable=true'
-   - 'traefik.http.routers.tv.entrypoints=plainhttp'
-   - 'traefik.http.services.tv.loadbalancer.server.port=8989'
-   - 'traefik.http.routers.tv.rule=Host("tv.[[PRIMARYDOMAIN]]")'
-   - 'traefik.http.middlewares.tv-force-secure.redirectscheme.scheme=https'
-   - 'traefik.http.routers.tv.middlewares=tv-force-secure'
-   - 'traefik.http.routers.tv.service=tv'
-   - 'traefik.http.routers.tv-https.entrypoints=encryptedhttp'
-   - 'traefik.http.routers.tv-https.rule=Host("tv.[[PRIMARYDOMAIN]]")'
-   - 'traefik.http.routers.tv-https.service=tv'
-   - 'traefik.http.routers.tv-https.tls=true'
-   - 'traefik.http.services.tv-https.loadbalancer.server.port=8989'
-   - 'com.MegaDocker.description=Sonarr - a tv episode search tool'
+  image: linuxserver/sonarr:latest
+  networks:
+   - traefik
+   - tv
+  volumes:
+   - ./media/tv-config:/config
+   - ./media/tv-data:/media
+  environment:
+   - PUID=$HOSTUSERID
+   - PGID=$HOSTUSERGID
+  deploy:
+   restart_policy:
+    condition: on-failure
+   labels:
+    - 'traefik.enable=true'
+    - 'traefik.http.routers.tv.entrypoints=plainhttp'
+     - 'traefik.http.services.tv.loadbalancer.server.port=8989'
+    - 'traefik.http.routers.tv.rule=Host("tv.[[PRIMARYDOMAIN]]")'
+    - 'traefik.http.middlewares.tv-force-secure.redirectscheme.scheme=https'
+    - 'traefik.http.routers.tv.middlewares=tv-force-secure'
+    - 'traefik.http.routers.tv.service=tv'
+    - 'traefik.http.routers.tv-https.entrypoints=encryptedhttp'
+    - 'traefik.http.routers.tv-https.rule=Host("tv.[[PRIMARYDOMAIN]]")'
+    - 'traefik.http.routers.tv-https.service=tv'
+    - 'traefik.http.routers.tv-https.tls=true'
+    - 'traefik.http.services.tv-https.loadbalancer.server.port=8989'
+    - 'com.MegaDocker.description=Sonarr - a tv episode search tool'
 
 # End TV Service Section
    
