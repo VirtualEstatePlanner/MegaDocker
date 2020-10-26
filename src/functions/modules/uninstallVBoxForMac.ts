0a270c58ba35bbfe7d1af781ce0f3ffa348b23f9
George Georgulas <agg4@Georges-MacBook-Pro.local>
2020-10-26 18:08:14 -0400

    chuff removal

--- src/functions/modules/uninstallVBoxForMac.ts
+++
@@ -1,9 +0,0 @@
-//  uninstallVBoxForMac.ts
-//  MegaDocker
-//  uninstalls Virtualbox
-//  Created by George Georgulas IV on 1/26/19.
-//  Copyright © 2019 The MegaDocker Group. All rights reserved.
-
-export function uninstallVBoxForMac() {
-  // TODO: runOrderedPrivileged the script from virtualbox's unisntall script
-}
