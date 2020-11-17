37283b948711dce9e6b7a8da1439cad5cf961c78
George Georgulas <agg4@Georges-MacBook-Pro.local>
2020-11-17 11:19:38 -0500

    added styling for button size

--- src/components/ButtonExportDockerSwarm.tsx
+++ src/components/ButtonExportDockerSwarm.tsx
@@ -19,6 +19,6 @@ export const ButtonExportDockerSwarm: React.FC = (): React.ReactElement => {
     const mobname: string = state.memories[mobnameIndex].value
 
     return (
-        <Button disabled={fullyValidated ? false : true} variant="contained" onClick={buttonClicked}>'download {mobname}.zip file'</Button>
+        <Button /*style={{maxHeight: '60px', minWidth: '300px'}}*/ disabled={fullyValidated ? false : true} variant="contained" onClick={buttonClicked}>'download {mobname}.zip'</Button>
     )
 }
