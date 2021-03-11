abbc117adee317b4d5a1b22ad24e51708d66ac59
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-11 15:53:36 -0500

    added basic serviceMite test

---
+++ src/interfaces/testTraefikedServiceMite.ts
@@ -0,0 +1,25 @@
+/** @format */
+
+//  testTraefikedServiceMite.ts
+//  MegaDocker
+//  an automated test
+//  Created by George Georgulas IV on 3/11/21.
+//  Copyright © 2021 The MegaDocker Group. All rights reserved.
+
+import { ITraefikedServiceMite } from './ITraefikedServiceMite'
+
+export const testTraefikedServiceMite: Function = (traefikedServiceMiteToTest: ITraefikedServiceMite) => {
+  it('has an index in the appropriate range', () => {
+    expect(traefikedServiceMiteToTest.miteIndex).toBeGreaterThanOrEqual(30000)
+    expect(traefikedServiceMiteToTest.miteIndex).toBeLessThanOrEqual(39999)
+  })
+  it('has the correct type', () => {
+    expect(traefikedServiceMiteToTest.type).toStrictEqual(`DockerSwarmService`)
+  })
+  it('has at least one host name', () => {
+    expect(traefikedServiceMiteToTest.webInterfaceHostnames.length).toBeGreaterThan(0)
+  })
+  it('has a valid miteString', () => {
+    expect(traefikedServiceMiteToTest.miteString).toBeDefined()
+  })
+}
