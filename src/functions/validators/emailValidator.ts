2dd4523530c658b3d085f3445e26c7a7a42b48b6
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-12 14:50:39 -0500

    removed unneccesary step

--- src/functions/validators/emailValidator.ts
+++ src/functions/validators/emailValidator.ts
@@ -2,7 +2,6 @@
 
 export const emailValidator: Function = (email: string): boolean => {
   const emailRegex: RegExp = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$')
-  const thisRegex: RegExp = emailRegex
-  const isValidated: boolean = thisRegex.test(email) ? true : false
+  const isValidated: boolean = emailRegex.test(email) ? true : false
   return isValidated
 }
