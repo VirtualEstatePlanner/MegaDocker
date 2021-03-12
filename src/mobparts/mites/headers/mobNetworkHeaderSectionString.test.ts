909c5a67781758d26425a87eaec609a26791ed01
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-12 18:48:33 -0500

    created tests

---
+++ src/mobparts/mites/headers/mobNetworkHeaderSectionString.test.ts
@@ -0,0 +1,12 @@
+/** @format */
+
+//  mobNetworkHeaderSectionString.test.ts
+//  MegaDocker
+//  an automated test
+//  Created by George Georgulas IV on 3/11/21.
+//  Copyright © 2021 The MegaDocker Group. All rights reserved.
+
+import { mobNetworkHeaderSectionString } from './mobNetworkHeaderSectionString'
+import { testHeaderMite } from '../../../tests/testHeaderMite'
+
+testHeaderMite(mobNetworkHeaderSectionString)
---
+++ src/mobparts/mites/headers/mobSecretsFooterSectionString.test.ts
@@ -0,0 +1,12 @@
+/** @format */
+
+//  mobSecretsFooterSectionString.test.ts
+//  MegaDocker
+//  an automated test
+//  Created by George Georgulas IV on 3/11/21.
+//  Copyright © 2021 The MegaDocker Group. All rights reserved.
+
+import { mobSecretsFooterSectionString } from './mobSecretsFooterSectionString'
+import { testHeaderMite } from '../../../tests/testHeaderMite'
+
+testHeaderMite(mobSecretsFooterSectionString)
