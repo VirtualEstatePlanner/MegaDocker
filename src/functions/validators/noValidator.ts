fbe4eae0e762d98345e494611d7cf19430c52937
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-13 12:32:28 -0500

    added console message to use a real validator

--- src/functions/validators/noValidator.ts
+++ src/functions/validators/noValidator.ts
@@ -6,6 +6,9 @@
 //  Created by George Georgulas IV on 1/26/19.
 //  Copyright © 2019 The MegaDocker Group. All rights reserved.
 
-export const noValidator: Function = (): boolean => {
+export const noValidator: Function = (stringToBeTested: string): boolean => {
+  console.log(`noValidator.ts: this validator function is only for use in development`)
+  console.log(`noValidator.ts: ${stringToBeTested} automatically returned true`)
+  console.log(`noValidator.ts: please use a real validator function`)
   return true
 }
