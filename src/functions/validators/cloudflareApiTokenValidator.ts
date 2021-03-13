9a63edd5e72cad3bdcf68efcd60b760bf5ec54aa
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-13 13:34:35 -0500

    removed force assertion of parameter string

--- src/functions/validators/cloudflareApiTokenValidator.ts
+++ src/functions/validators/cloudflareApiTokenValidator.ts
@@ -10,7 +10,7 @@ export const cloudflareApiTokenValidator: Function = (stringToValidate: string):
   if (!stringToValidate) {
     return false
   }
-  const isExactlyFortyCharacters: boolean = stringToValidate!.length === 40
+  const isExactlyFortyCharacters: boolean = stringToValidate.length === 40
   const isAlphanumericRegex: RegExp = new RegExp(`[a-zA-Z0-9_-]`)
   const containsOnlyAlphanumerics: boolean = isAlphanumericRegex.test(stringToValidate!) ? true : false
   const isValidated: boolean = containsOnlyAlphanumerics && isExactlyFortyCharacters
--- src/functions/validators/emailValidator.ts
+++ src/functions/validators/emailValidator.ts
@@ -12,6 +12,6 @@ export const emailValidator: Function = (stringToValidate: string): boolean => {
     return false
   }
   const emailRegex: RegExp = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$')
-  const isValidated: boolean = emailRegex.test(stringToValidate!) ? true : false
+  const isValidated: boolean = emailRegex.test(stringToValidate) ? true : false
   return isValidated
 }
