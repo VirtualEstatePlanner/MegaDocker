67e226c6d5881b2f12d33b1bac66b275fc7ca4f9
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-13 12:31:51 -0500

    changed to test for single condition

--- src/tests/test-functions/testValidator.ts
+++ src/tests/test-functions/testValidator.ts
@@ -6,12 +6,8 @@
 //  Created by George Georgulas IV on 3/11/21.
 //  Copyright © 2021 The MegaDocker Group. All rights reserved.
 
-import { IValidator } from '../../interfaces/IValidator'
-
-export const testValidator: Function = (testStringArray: string[], validatorToTest: IValidator) => {
-  testStringArray.forEach((stringToBeTested: string) => {
-    it('should return true', () => {
-      expect(validatorToTest.isValidated).toStrictEqual(true)
-    })
+export const testValidator: Function = (stringToBeTested: string, validatorToTest: (stringToValidate: string) => boolean) => {
+  it('should be truthy', () => {
+    expect(validatorToTest(stringToBeTested)).toBeTruthy()
   })
 }
