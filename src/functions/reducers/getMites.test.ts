f641a4ebeff4ac0a3ebe19c96019943f4134ae7f
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-12 22:19:48 -0500

    created test skeletons for functions/reducers

---
+++ src/functions/reducers/getMites.test.ts
@@ -0,0 +1,9 @@
+/** @format */
+
+//  getMites.test.ts
+//  MegaDocker
+//  an automated test
+//  Created by George Georgulas IV on 3/12/21.
+//  Copyright © 2021 The MegaDocker Group. All rights reserved.
+
+import { getMites } from './getMites'
