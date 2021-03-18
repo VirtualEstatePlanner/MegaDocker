a6c13c99d7cc16674c62983a2bf6166407d6d261
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-18 00:18:55 -0400

    removed silly imports pile

--- src/tests/functions/reducers/zipDockerSwarm.test.ts
+++ src/tests/functions/reducers/zipDockerSwarm.test.ts
@@ -6,36 +6,9 @@
 //  Created by George Georgulas IV on 3/12/21.
 //  Copyright © 2021 The MegaDocker Group. All rights reserved.
 
-import { MegaContext } from '../../../components/MegaContext'
-import { getCustomMites } from '../../../functions/reducers/getCustomMites'
-import { getDNetworkMites } from '../../../functions/reducers/getDNetworkMites'
-import { getDServiceMites } from '../../../functions/reducers/getDServiceMites'
-import { getManikins } from '../../../functions/reducers/getManikins'
-import { getMemories } from '../../../functions/reducers/getMemories'
-import { getMites } from '../../../functions/reducers/getMites'
-import { workingManikins } from '../../../globals/workingManikins'
-import { IMegaDockerState } from '../../../interfaces/IMegaDockerState'
+import { MegaProvider, initialMegaDockerState } from '../../../components/MegaContext'
 import { testMegaDockerAction } from '../../test-functions/testMegaDockerAction'
 import { testMegaDockerState } from '../../test-functions/testMegaDockerState'
-
-const selectedManikins = getManikins(workingManikins)
-const allMobMites = getMites(selectedManikins)
-const memories = getMemories(selectedManikins)
-const mobDServiceMites = getDServiceMites(allMobMites)
-const mobDNetworkMites = getDNetworkMites(allMobMites)
-const mobCustomMites = getCustomMites(allMobMites)
-const ymlOutput = `testingState`
-
-const testingState: IMegaDockerState = {
-  manikinTable: workingManikins,
-  selectedManikins: selectedManikins,
-  memories: memories,
-  allMobMites: allMobMites,
-  mobDServiceMites: mobDServiceMites,
-  mobDNetworkMites: mobDNetworkMites,
-  mobCustomMites: mobCustomMites,
-  ymlOutput: ymlOutput,
-}
 
 describe('tests the zipDockerCompose function', () => {
   testMegaDockerAction
