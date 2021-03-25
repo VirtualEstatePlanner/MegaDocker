452bcb8333a2640ff07d07068583a32509864136
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-25 09:31:45 -0400

    mostly correct, just need layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,6 +7,7 @@
 //  Copyright © 2019-2021 The MegaDocker Group. All rights reserved.
 
 import * as React from 'react'
+import { Paper } from '@material-ui/core'
 import { ContainerManikinPane } from './ContainerManikinPane'
 import { ContainerMainArea } from './ContainerMainArea'
 import { MegaProvider } from './MegaContext'
@@ -15,8 +16,10 @@ export const App: React.FC = (): React.ReactElement => {
   return (
     <div className='App'>
       <MegaProvider>
-        <ContainerManikinPane />
-        <ContainerMainArea />
+        <Paper>
+          <ContainerManikinPane />
+          <ContainerMainArea />
+        </Paper>
       </MegaProvider>
     </div>
   )
