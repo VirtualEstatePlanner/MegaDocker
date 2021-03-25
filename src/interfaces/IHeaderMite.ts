5ef6d6b684914b495859e4866c0ea998740b7904
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-25 19:31:27 -0400

    moved import below file header

--- src/interfaces/IHeaderMite.ts
+++ src/interfaces/IHeaderMite.ts
@@ -1,12 +1,12 @@
 /** @format */
 
-import { IMite } from './IMite'
-
 //  IHeaderMite.ts
 //  MEGADocker
 //  an interface that represents a Mite with a Docker swarm Network
 //  Created by George Georgulas IV on 3/14/21.
 //  Copyright © 2021 The MegaDocker Group. All rights reserved.
+
+import { IMite } from './IMite'
 
 /**
  * interface for INetworkMite
