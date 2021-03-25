858035ac23ac21e4af0bb9b2aad4c68f7f1f0cd4
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-25 09:08:41 -0400

    moved initialMegaDockerState to globals

--- src/components/MegaContext.tsx
+++ src/components/MegaContext.tsx
@@ -6,30 +6,10 @@
 //  Created by George Georgulas IV on 3/17/19.
 //  Copyright © 2019-2021 The MegaDocker Group. All rights reserved.
 
-import React from 'react'
-import { IMegaDockerState } from '../interfaces/IMegaDockerState'
-import { IManikin } from '../interfaces/IManikin'
-import { IMite } from '../interfaces/IMite'
-import { workingManikins } from '../globals/workingManikins'
+import * as React from 'react'
 import { megaReducer } from '../functions/reducers/megaReducer'
-import { OrbThemeLight } from './OrbThemeLight'
 import { ThemeProvider } from '@material-ui/core'
-
-const coreManikins: IManikin[] = workingManikins.filter((eachManikin: IManikin) => (eachManikin.isCore ? true : false))
-const otherManikins: IManikin[] = workingManikins.filter((eachManikin: IManikin) => (eachManikin.isCore ? false : true))
-const initialMobMites: IMite[] = coreManikins.flatMap((eachManikin) => eachManikin.mites.flatMap((eachMite) => eachMite))
-
-export const initialMegaDockerState: IMegaDockerState = {
-  theme: OrbThemeLight,
-  manikinTable: [...coreManikins, ...otherManikins],
-  selectedManikins: coreManikins,
-  memories: coreManikins.flatMap((eachManikin) => eachManikin.memories),
-  allMobMites: initialMobMites,
-  mobDServiceMites: initialMobMites.filter((eachMite) => eachMite.type === `DockerSwarmService`),
-  mobDNetworkMites: initialMobMites.filter((eachMite) => eachMite.type === `DockerSwarmNetwork`),
-  mobCustomMites: [initialMobMites.filter((eachMite) => eachMite.type === `Custom`)],
-  ymlOutput: ``,
-}
+import { initialMegaDockerState } from '../globals/initialMegaDockerState'
 
 export const MegaContext: React.Context<any> = React.createContext({
   state: initialMegaDockerState,
