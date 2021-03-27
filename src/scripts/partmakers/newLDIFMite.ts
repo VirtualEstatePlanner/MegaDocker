ea2d22feb9744c85127ec1659cfdcca5e155ca28
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-27 13:44:15 -0400

    corrected LDIF script filename

--- src/scripts/partmakers/newLDIFMite.ts
+++ src/scripts/partmakers/newLDIFMite.ts
@@ -17,57 +17,59 @@ const getLdifMiteData = readline.createInterface({
   output: process.stdout,
 })
 
-const makeNetworkMiteFileString: Function = (): string => {
+const makeLdifMiteFileString: Function = (input: ILDIFMiteInput): string => {
   const date: Date = new Date()
   const shortDate = date.toLocaleString().split(',')[0]
   const year: number = date.getFullYear()
   const template: string = `/** @format */
 
-//  ${runtimeLdifMiteAnswers.serviceName}LdifMite.ts
+//  ${input.serviceName}LdifMite.ts
 //  MEGADocker
-//  LDIF Mite for ${runtimeLdifMiteAnswers.serviceName}
+//  LDIF Mite for ${input.serviceName}
 //  Created by George Georgulas IV on ${shortDate}.
 //  Copyright © ${year} The MegaDocker Group. All rights reserved.
 
 import { ILDIFMite } from '../../../interfaces/ILDIFMite'
 
-export const ${runtimeLdifMiteAnswers.serviceName}LdifMite: ILDIFMite = {
+export const ${input.serviceName}LdifMite: ILDIFMite = {
   type: \`LDIF\`,
   miteIndex: 99999,
-  miteString: \`# Begin ${runtimeLdifMiteAnswers.serviceName} Section
+  miteString: \`# Begin ${input.serviceName} Section
 
-dn: ou=${runtimeLdifMiteAnswers.serviceName}Users,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
+dn: ou=${input.serviceName}Users,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
 objectclass: organizationalUnit
 
-dn: ou=${runtimeLdifMiteAnswers.serviceName}Admins,ou=${runtimeLdifMiteAnswers.serviceName}Users,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
+dn: ou=${input.serviceName}Admins,ou=${input.serviceName}Users,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
 objectclass: organizationalUnit
 
-dn: ou=${runtimeLdifMiteAnswers.serviceName}Moderators,ou=${runtimeLdifMiteAnswers.serviceName}Users,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
+dn: ou=${input.serviceName}Moderators,ou=${input.serviceName}Users,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
 objectclass: organizationalUnit
 
-dn: ou=${runtimeLdifMiteAnswers.serviceName}Admins,ou=Administrators,ou=Technicians,ou=Employees,[[LDAPDOMAINASDCS]]
+dn: ou=${input.serviceName}Admins,ou=Administrators,ou=Technicians,ou=Employees,[[LDAPDOMAINASDCS]]
 objectclass: alias
 objectclass: extensibleobject
-ou: ${runtimeLdifMiteAnswers.serviceName}Admins
-aliasedObjectName: ou=${runtimeLdifMiteAnswers.serviceName}ITAdmins,ou=${runtimeLdifMiteAnswers.serviceName}Admins,ou=${runtimeLdifMiteAnswers.serviceName}Users,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
+ou: ${input.serviceName}Admins
+aliasedObjectName: ou=${input.serviceName}ITAdmins,ou=${input.serviceName}Admins,ou=${input.serviceName}Users,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
 
 dn: ou=${runtimeLdifMiteAnswers.serviceName}Moderators,ou=Moderators,ou=Technicians,ou=Employees,[[LDAPDOMAINASDCS]]
 objectclass: alias
 objectclass: extensibleobject
-ou: ${runtimeLdifMiteAnswers.serviceName}Moderators
-aliasedObjectName: ou=${runtimeLdifMiteAnswers.serviceName}ITModerators,ou=${runtimeLdifMiteAnswers.serviceName}Moderators,ou=${runtimeLdifMiteAnswers.serviceName}Users,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
+ou: ${input.serviceName}Moderators
+aliasedObjectName: ou=${input.serviceName}ITModerators,ou=${input.serviceName}Moderators,ou=${input.serviceName}Users,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
 
-# End ${runtimeLdifMiteAnswers.serviceName} Section
+# End ${input.serviceName} Section
 \`}`
 
   return template
 }
 
-getLdifMiteData.question(`What is the name of this service? (PascalCase):`, function (serviceName: string) {
+getLdifMiteData.question(`What is the name of this LDIF-secured service? (PascalCase):`, function (serviceName: string) {
   runtimeLdifMiteAnswers.serviceName = serviceName
   getLdifMiteData.close()
 
-  const outputNewManikinString: string = makeNetworkMiteFileString(runtimeLdifMiteAnswers)
+  const lowercaseName: string = runtimeLdifMiteAnswers.serviceName.toLowerCase()
 
-  fs.writeFileSync(`src/mobparts/mites/ldif/${runtimeLdifMiteAnswers.serviceName}LdifMite.ts`, outputNewManikinString)
+  const outputNewLdifMiteString: string = makeLdifMiteFileString(runtimeLdifMiteAnswers)
+
+  fs.writeFileSync(`src/mobparts/mites/ldif/${lowercaseName}LdifMite.ts`, outputNewLdifMiteString)
 })
