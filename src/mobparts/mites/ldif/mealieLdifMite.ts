3857ef31a32914f53d1c2539b564ac4f5682e726
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-27 11:32:50 -0400

    created mealie ldif mite

--- src/mobparts/mites/ldif/mealieLdifMite.ts
+++ src/mobparts/mites/ldif/mealieLdifMite.ts
@@ -11,5 +11,30 @@ import { ILDIFMite } from '../../../interfaces/ILDIFMite'
 export const mealieLdifMite: ILDIFMite = {
   type: `LDIF`,
   miteIndex: 50016,
-  miteString: ``,
+  miteString: `# Begin Mealie Section
+
+dn: ou=MealieUsers,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=MealieAdmins,ou=MealieUsers,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=MealieModerators,ou=MealieUsers,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=MealieAdmins,ou=Administrators,ou=Technicians,ou=Employees,[[LDAPDOMAINASDCS]]
+objectclass: alias
+objectclass: extensibleobject
+ou: MealieAdmins
+aliasedObjectName: ou=MealieITAdmins,ou=MealieAdmins,ou=MealieUsers,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
+
+dn: ou=MealieModeratorss,ou=Moderators,ou=Technicians,ou=Employees,[[LDAPDOMAINASDCS]]
+objectclass: alias
+objectclass: extensibleobject
+ou: MealieModerators
+aliasedObjectName: ou=MealieITModerators,ou=MealieModerators,ou=MealieUsers,ou=InternalUsers,ou=Employees,[[LDAPDOMAINASDCS]]
+
+# End Mealie Section
+
+`,
 }
