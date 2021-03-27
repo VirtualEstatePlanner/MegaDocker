40853d7ed6726d071afd8418cbc889ac02cbe623
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-27 11:27:17 -0400

    added Hardware section to ldif base structure

--- src/mobparts/mites/custom/ldapBootstrapMegaDockerDotLdifMite.ts
+++ src/mobparts/mites/custom/ldapBootstrapMegaDockerDotLdifMite.ts
@@ -69,6 +69,202 @@ objectclass: organizationalUnit
 
 # End Employees Section
 
+# Begin Hardware Section
+
+dn: ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Batteries,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Linux,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=AMD64,ou=Linux,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Desktops,ou=AMD64,ou=Linux,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Laptops,ou=AMD64,ou=Linux,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Servers,ou=AMD64,ou=Linux,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=ARM64,ou=Linux,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Desktops,ou=ARM64,ou=Linux,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Laptops,ou=ARM64,ou=Linux,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Servers,ou=ARM64,ou=Linux,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=MacOS,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=AMD64,ou=MacOS,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Desktops,ou=AMD64,ou=MacOS,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Laptops,ou=AMD64,ou=MacOS,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Servers,ou=AMD64,ou=MacOS,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=ARM64,ou=MacOS,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Desktops,ou=ARM64,ou=MacOS,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Laptops,ou=ARM64,ou=MacOS,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Servers,ou=ARM64,ou=MacOS,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Windows,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=AMD64,ou=Windows,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Desktops,ou=AMD64ou=Windows,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Laptops,ou=AMD64ou=Windows,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Servers,ou=AMD64ou=Windows,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=ARM64,ou=Windows,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Desktops,ou=ARM64,ou=Windows,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Laptops,ou=ARM64,ou=Windows,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Servers,ou=ARM64,ou=Windows,ou=Computers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=MobileDevices,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=AndroidDevices,ou=MobileDevices,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Phones,ou=AndroidDevices,ou=MobileDevices,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Tablets,ou=AndroidDevices,ou=MobileDevices,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Wearables,ou=AndroidDevices,ou=MobileDevices,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=AppleDevices,ou=MobileDevices,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Phones,ou=AppleDevices,ou=MobileDevices,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Tablets,ou=AppleDevices,ou=MobileDevices,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Wearables,ou=AppleDevices,ou=MobileDevices,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=NetworkGear,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Firewalls,ou=NetworkGear,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Routers,ou=NetworkGear,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Switches,ou=NetworkGear,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=WirelessAccessPoints,ou=NetworkGear,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Printers,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=VOIPPhones,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Security,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Cameras,ou=Security,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Doorbells,ou=Security,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Sensors,ou=Security,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=CarbonMonoxideDetectors,ou=Sensors,ou=Security,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=DoorSensors,ou=Sensors,ou=Security,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=GlassBreakSensors,ou=Sensors,ou=Security,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=SmokeDetectors,ou=Sensors,ou=Security,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=WindowSensors,ou=Sensors,ou=Security,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=SettopBoxes,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=AppleTVs,ou=SettopBoxes,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=FireTVSticks,ou=SettopBoxes,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=Rokus,ou=SettopBoxes,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=SmartTVs,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=RokuTVs,ou=SmartTVs,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=FireTVs,ou=SmartTVs,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=LGTVs,ou=SmartTVs,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+dn: ou=SamsungTVs,ou=SmartTVs,ou=Hardware,[[LDAPDOMAINASDCS]]
+objectclass: organizationalUnit
+
+# End Hardwware Section
+
 # Begin Users Section
 
 dn: ou=Users,[[LDAPDOMAINASDCS]]
