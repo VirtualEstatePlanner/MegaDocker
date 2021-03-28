fcff0e0a86e580e32f8d22a0da0025274bb84103
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-28 02:22:16 -0400

    added file header generation

--- src/scripts/partmakers/newCustomMite.ts
+++ src/scripts/partmakers/newCustomMite.ts
@@ -8,12 +8,14 @@ interface ICustomMiteInput {
   filePath: string
   fileName: string
   fileExtension: string
+  fileDescription: string
   filePermissions: number
 }
 
 let runtimeCustomMiteAnswers: ICustomMiteInput = {
   filePath: ``,
   fileName: ``,
+  fileDescription: ``,
   fileExtension: ``,
   filePermissions: 0,
 }
@@ -29,13 +31,16 @@ getManikinData.question(`What is the path to this file's location?: `, function 
     runtimeCustomMiteAnswers.fileName = fileName
     getManikinData.question(`What is this file's extension? (conf, config, db, html, js, json, ldif, options, properties, sh, txt, or yml): `, function (fileExtension: string) {
       runtimeCustomMiteAnswers.fileExtension = fileExtension
-      getManikinData.question(`What permissions should this file have? (3-digits between 0 and 7 number, such as 644): `, function (filePermissions: string) {
-        runtimeCustomMiteAnswers.filePermissions = parseInt(filePermissions)
-        getManikinData.close()
+      getManikinData.question(`What is a short description of this file's purpose?: `, function (fileDescription: string) {
+        runtimeCustomMiteAnswers.fileDescription = fileDescription
+        getManikinData.question(`What permissions should this file have? (3-digits between 0 and 7 number, such as 644): `, function (filePermissions: string) {
+          runtimeCustomMiteAnswers.filePermissions = parseInt(filePermissions)
+          getManikinData.close()
 
-        const outputNewManikinString: string = makeFileString(runtimeCustomMiteAnswers)
+          const outputNewManikinString: string = makeFileString(runtimeCustomMiteAnswers)
 
-        fs.writeFileSync(`src/mobparts/mites/custom/${runtimeCustomMiteAnswers.fileName}Mite.ts`, outputNewManikinString)
+          fs.writeFileSync(`src/mobparts/mites/custom/${runtimeCustomMiteAnswers.fileName}Mite.ts`, outputNewManikinString)
+        })
       })
     })
   })
@@ -46,6 +51,12 @@ const makeFileString: Function = (options: ICustomMiteInput): string => {
   const shortDate = date.toLocaleString().split(',')[0]
   const year: number = date.getFullYear()
   const template: string = `/** @format */
+
+//  ${options.fileName}Mite.ts
+//  MEGADocker
+//  ${options.fileDescription}
+//  Created by George Georgulas IV on ${shortDate}.
+//  Copyright © ${year} The MegaDocker Group. All rights reserved.
 
 import { ICustomMite } from '../../../interfaces/ICustomMite'
 
