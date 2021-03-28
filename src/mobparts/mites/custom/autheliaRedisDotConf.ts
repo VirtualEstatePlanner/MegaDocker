3ed5a6fe9635b1bddf949c32370cf657a3e5decc
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-27 23:10:50 -0400

    created mite skeleton

---
+++ src/mobparts/mites/custom/autheliaRedisDotConf.ts
@@ -0,0 +1,16 @@
+/** @format */
+
+import { ICustomMite } from '../../../interfaces/ICustomMite'
+
+export const filebrowserDatabaseDotDbMite: ICustomMite = {
+  type: `Custom`,
+  miteString: ``,
+  miteIndex: 99999,
+  miteFile: {
+    path: `ldap/authelia-redis-conf`,
+    name: `autheliaRedisDotConf`,
+    extension: `conf`,
+    permissions: `644`,
+    contents: ``,
+  },
+}
