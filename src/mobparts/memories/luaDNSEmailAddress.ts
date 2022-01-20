86e105ec5f22192901e073c26c1a0f9a6e08c494
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2022-01-19 20:19:51 -0500

    created luaDNS memories

---
+++ src/mobparts/memories/luaDNSEmailAddress.ts
@@ -0,0 +1,23 @@
+/** @format */
+
+//  luaDNSEmailAddress.ts
+//  MEGADocker
+//  The variable for the LuaDNS username
+//  Created by George Georgulas IV on 10/20/21.
+//  Copyright © 2021 The MegaDocker Group. All rights reserved.
+
+import { IMemory } from '../../interfaces/objectInterfaces/IMemory'
+import { emailValidator } from '../../functions/validators/emailValidator'
+
+export const luaDNSEmailAddress: IMemory = {
+  memoryIndex: 20028,
+  memoryMarker: `[[LUADNSEMAILADDRESS]]`,
+  memoryType: `NormalMemory`,
+  shouldAutocomplete: false,
+  name: `LuaDNS Email Address`,
+  tooltip: `Your LuaDNS Email address`,
+  value: ``,
+  valueType: `text`,
+  isReady: false,
+  validator: emailValidator,
+}
