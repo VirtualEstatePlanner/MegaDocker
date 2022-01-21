a887694e844d3a476332ca8653f8acf8310ccfb4
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2022-01-20 19:06:13 -0500

    removed node fs calls

--- src/functions/validators/stringYamlValidator.ts
+++ src/functions/validators/stringYamlValidator.ts
@@ -7,29 +7,24 @@
 //  Copyright © 2021 The MegaDocker Group. All rights reserved.
 
 import YamlValidator from 'yaml-validator'
-import fs from 'fs'
 
 export const stringYamlValidator: Function = (stringToValidate: string): boolean => {
-  const temporaryYamlFileName: string = `/tmp/YamlValidator/mite-somestring-Testfile.yml`
-  if (!fs.existsSync(`/tmp/YamlValidator`)) {
-    fs.mkdirSync(`/tmp/YamlValidator`)
-  }
-  fs.writeFileSync(temporaryYamlFileName, stringToValidate, { encoding: 'utf8' })
 
-  class YamlValidatorForTesting extends YamlValidator {
-    testValidation: Function = (files: string[]): boolean => {
-      let validatePassed: boolean
-      try {
-        super.validate(files)
-        validatePassed = true
-      } catch (error) {
-        validatePassed = false
-      }
-      return validatePassed
+class YamlValidatorForTesting extends YamlValidator {
+  testValidation: Function = (yamlStrings: string[]): boolean => {
+    let validatePassed: boolean = false
+    try {
+      super.validate(yamlStrings)
+      validatePassed = true
+    } catch (error) {
+      validatePassed = false
     }
+    return validatePassed
   }
-  const validator: YamlValidatorForTesting = new YamlValidatorForTesting()
+}
 
-  const isValidated = validator.testValidation([temporaryYamlFileName])
-  return isValidated
+const validator: YamlValidatorForTesting = new YamlValidatorForTesting()
+
+const isValidated = validator.testValidation([stringToValidate])
+return isValidated
 }
