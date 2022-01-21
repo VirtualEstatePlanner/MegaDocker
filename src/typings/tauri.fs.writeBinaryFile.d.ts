671b7a030d9f9f31783ccc1b08c09b13fd10ee63
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2022-01-20 19:06:01 -0500

    tauri.fs.writeBinaryFile.cjs declaration file

---
+++ src/typings/tauri.fs.writeBinaryFile.d.ts
@@ -0,0 +1,12 @@
+/** @format */
+
+declare module '@tauri-apps/api/fs.cjs'
+
+/**
+ * Writes a binary file.
+ *
+ * @param file Write configuration object.
+ * @param options Configuration object.
+ * @returns A promise indicating the success or failure of the operation.
+ */
+ declare function writeBinaryFile(file: FsBinaryFileOption, options?: FsOptions): Promise<void>;
