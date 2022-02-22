c5e59df3dade443f0b037ee5a3ff6f3a343c63ca
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2022-02-22 15:08:12 -0500

    oh that file too

--- src/functions/reducers/saveMobBrowser.ts
+++ src/functions/reducers/saveMobBrowser.ts
@@ -15,7 +15,19 @@ import { mobName } from '../../mobparts/memories/mobName'
  * saves a .mob file in the browser
  */
 export const saveMobBrowser: Function = (state: IMegaDockerState) => {
-  const blob: Blob = new Blob(convertMDStateToJson(state), { type: `application/json` })
+  // convert state to json
+    const JSONState: JSON = convertMDStateToJson(state)
+  // convert json to a string
+    const JSONString: string = JSON.stringify(JSONState)
+  // convert json to blob
+  const blob: Blob = new Blob([JSONString], { type: `application/json` })
   const mobNameValue: string = state.memories[state.memories.indexOf(mobName)].value
-  fileSaver.saveAs(blob, `${mobNameValue}.mob`)
+  // check for existence of mobNameValue
+  if (mobNameValue) {
+    // save the blob
+    fileSaver.saveAs(blob, `${mobNameValue}.mob`)
+  } else {
+    // save the blob
+    fileSaver.saveAs(blob, `untitled.mob`)
+  }
 }
