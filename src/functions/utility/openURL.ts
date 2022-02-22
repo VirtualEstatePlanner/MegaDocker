b54aac44ef6a15785ec0652734eacdf5493264aa
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2022-02-22 12:30:39 -0500

    removed console.logs and added comments

--- src/functions/utility/openURL.ts
+++ src/functions/utility/openURL.ts
@@ -2,7 +2,7 @@
 
 //  openURL.ts
 //  MEGADocker
-//  a function that opens a URL in a new tab in the current browser or the default browser
+//  a function that opens a URL
 //  Created by George Georgulas IV on 2/22/22.
 //  Copyright © 2022 The MegaDocker Group. All rights reserved.
 
@@ -10,16 +10,14 @@ import { runningInTauri } from "./runningInTauri"
 import { open } from '@tauri-apps/api/shell'
 
 /**
- * updates allMobMites array based on application state
+ * opens a URL in a new tab from the current browser or in the default browser from Tauri
  */
 export const openURL: Function = (url: URL): void => {
-    console.log(`clicked button to open docker url`)
     if (runningInTauri() === true) {
       open(url.href)
     } else {
       window.open(url.href, `_blank`)    
     }
-    console.log(`theoretically opened url`)
     return
   }
   
