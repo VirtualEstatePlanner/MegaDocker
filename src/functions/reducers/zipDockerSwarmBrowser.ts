a6d46f4f85506772815150d979ca712fd2297ee3
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2022-02-22 15:14:51 -0500

    added type to content in fileSaver.saveAs call

--- src/functions/reducers/zipDockerSwarmBrowser.ts
+++ src/functions/reducers/zipDockerSwarmBrowser.ts
@@ -6,7 +6,7 @@
 //  Created by George Georgulas IV on 1/26/19.
 //  Copyright © 2019-2022 The MegaDocker Group. All rights reserved.
 
-import JSZip from 'jszip'
+import { cloudflareAPIToken } from '../../mobparts/memories/cloudflareAPIToken'
 import fileSaver from 'file-saver'
 import { IMemory } from '../../interfaces/objectInterfaces/IMemory'
 import { IManikin } from '../../interfaces/objectInterfaces/IManikin'
@@ -15,17 +15,17 @@ import { ILDIFMite } from '../../interfaces/miteTypeInterfaces/ILDIFMite'
 import { ICustomMite } from '../../interfaces/miteTypeInterfaces/ICustomMite'
 import { ITraefikedServiceMite } from '../../interfaces/miteTypeInterfaces/ITraefikedServiceMite'
 import { IZipDockerCompose } from '../../interfaces/stateManagement/IZipDockerCompose'
+import JSZip from 'jszip'
+import { ldapBootstrapMegaDockerDotLdifMite } from '../../mobparts/mites/custom/ldapBootstrapMegaDockerDotLdifMite'
 import { mobFileHeaderSectionString } from '../../mobparts/mites/headers/mobFileHeaderSectionString'
 import { mobServicesFooterSectionString } from '../../mobparts/mites/headers/mobServicesFooterSectionString'
+import { mobName } from '../../mobparts/memories/mobName'
 import { mobNetworkHeaderSectionString } from '../../mobparts/mites/headers/mobNetworkHeaderSectionString'
 import { mobNetworkFooterSectionString } from '../../mobparts/mites/headers/mobNetworkFooterSectionString'
-import { traefikManikin } from '../../mobparts/manikins/traefik'
-import { mobName } from '../../mobparts/memories/mobName'
-import { ldapBootstrapMegaDockerDotLdifMite } from '../../mobparts/mites/custom/ldapBootstrapMegaDockerDotLdifMite'
-import { primaryDomain } from '../../mobparts/memories/primaryDomain'
-import { cloudflareAPIToken } from '../../mobparts/memories/cloudflareAPIToken'
 import { mobSecretsHeaderSectionString } from '../../mobparts/mites/headers/mobSecretsHeaderSectionString'
 import { mobSecretsFooterSectionString } from '../../mobparts/mites/headers/mobSecretsFooterSectionString'
+import { primaryDomain } from '../../mobparts/memories/primaryDomain'
+import { traefikManikin } from '../../mobparts/manikins/traefik'
 
 /**
  * makes .zip file for docker-compose in web browser
@@ -320,7 +320,7 @@ fi;
       platform: `UNIX`,
       type: `blob`
     })
-    .then(function (content) {
+    .then(function (content: Blob) {
       fileSaver.saveAs(content, `${zipManikins[traefikIndex].memories[mobNameIndex].value}.zip`)
     })
 }
