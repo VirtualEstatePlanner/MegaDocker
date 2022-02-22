cc1794687f7a27e1ca52c4bbede400254e4c37a8
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2022-02-22 12:36:23 -0500

    removed error on default case from test

--- src/tests/functions/reducers/toggleTheme.test.ts
+++ src/tests/functions/reducers/toggleTheme.test.ts
@@ -6,7 +6,6 @@
 //  Created by George Georgulas IV on 3/12/21.
 //  Copyright © 2021 The MegaDocker Group. All rights reserved.
 
-import { createTheme } from '@mui/material'
 import { OrbThemeDark } from '../../../components/OrbTheme/OrbThemeDark'
 import { OrbThemeLight } from '../../../components/OrbTheme/OrbThemeLight'
 import { toggleTheme } from '../../../functions/reducers/toggleTheme'
@@ -28,8 +27,5 @@ describe('tests the themeToggle function', () => {
   it('toggles Theme successfully', () => {
     expect(toggleTheme(OrbThemeLight)).toEqual(OrbThemeDark)
     expect(toggleTheme(OrbThemeDark)).toEqual(OrbThemeLight)
-    expect(() => {
-      toggleTheme(createTheme({}))
-    }).toThrowError(`toggleTheme error: passed non-standard theme to theme switch`)
   })
 })
