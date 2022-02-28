dd299aa6d0f15e0dffa0d56590e5d2d399bd49d3
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2022-02-28 10:16:16 -0500

    var -> let because no using var

--- src/mobparts/mites/custom/vsCodeIndexDotHtmlMite.ts
+++ src/mobparts/mites/custom/vsCodeIndexDotHtmlMite.ts
@@ -68,7 +68,7 @@ export const vsCodeIndexDotHtmlMite: ICustomMite = {
     <script>
         window.addEventListener("load", function() {
             if (window._noVNC_has_module_support) return;
-            var loader = document.createElement("script");
+            let loader = document.createElement("script");
             loader.src = "vendor/browser-es-module-loader/dist/browser-es-module-loader.js";
             document.head.appendChild(loader);
         });
