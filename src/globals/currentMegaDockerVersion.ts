14f617707f5391a1e80f711b6f7c57389b876a09
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2022-03-01 01:26:03 -0500

    created current version for save file versioning

---
+++ src/globals/currentMegaDockerVersion.ts
@@ -0,0 +1,9 @@
+/** @format */
+
+//  currentVersion.ts
+//  MEGADocker
+//  a convenience global string constant with the current version of MEGADocker
+//  Created by George Georgulas IV on 3/17/19.
+//  Copyright © 2019-2022 The MegaDocker Group. All rights reserved.
+
+export const currentMegaDockerVersion: string = `beta-0.5.1`
